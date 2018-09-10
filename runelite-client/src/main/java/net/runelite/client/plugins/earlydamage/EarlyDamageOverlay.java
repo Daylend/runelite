91ef0f32874d6e9c0ec0e94c91c727653f02e92a
Daylend <daylend@degras.se>
2018-09-10 00:05:05 -0600

    fuck

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/earlydamage/EarlyDamageOverlay.java
@@ -0,0 +1,48 @@
+package net.runelite.client.plugins.earlydamage;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
+import java.time.Instant;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.Experience;
+import net.runelite.api.Point;
+import net.runelite.client.game.SkillIconManager;
+import net.runelite.client.plugins.xptracker.XpTrackerService;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+import net.runelite.client.ui.overlay.components.ProgressBarComponent;
+
+public class EarlyDamageOverlay extends Overlay
+{
+    private final Client client;
+    private final EarlyDamagePlugin plugin;
+    private final EarlyDamageConfig config;
+    private final XpTrackerService xpTrackerService;
+
+    @Inject
+    private EarlyDamageOverlay(
+            Client client,
+            EarlyDamagePlugin plugin,
+            EarlyDamageConfig config,
+            XpTrackerService xpTrackerService,
+    {
+        this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+        this.xpTrackerService = xpTrackerService;
+        setPosition(OverlayPosition.TOP_CENTER);
+    }
+    )
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/earlydamage/EarlyDamagePlugin.java
@@ -0,0 +1,93 @@
+package net.runelite.client.plugins.earlydamage;
+
+
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Provides;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.Experience;
+import net.runelite.api.Skill;
+import net.runelite.api.events.ExperienceChanged;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDependency;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.xptracker.XpTrackerPlugin;
+import net.runelite.client.task.Schedule;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+@PluginDescriptor(
+        name = "XP Globes",
+        description = "Show XP globes for the respective skill when gaining XP",
+        tags = {"experience", "levels", "overlay"},
+        enabledByDefault = false
+)
+@PluginDependency(XpTrackerPlugin.class)
+public class EarlyDamagePlugin extends Plugin
+{
+    private static final int MAXIMUM_SHOWN_GLOBES = 5;
+
+    @Inject
+    private Client client;
+
+    @Inject
+    private EarlyDamageConfig config;
+
+    @Inject
+    private OverlayManager overlayManager;
+
+    @Inject
+    private EarlyDamageOverlay overlay;
+
+    @Provides
+    EarlyDamageConfig getConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(EarlyDamageConfig.class);
+    }
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        overlayManager.add(overlay);
+    }
+
+    @Override
+    protected void shutDown() throws Exception
+    {
+        overlayManager.remove(overlay);
+    }
+
+    @Subscribe
+    public void onExperienceChanged(ExperienceChanged event)
+    {
+        Skill skill = event.getSkill();
+        int currentXp = client.getSkillExperience(skill);
+        int currentLevel = Experience.getLevelForXp(currentXp);
+        int skillIdx = skill.ordinal();
+        //XpGlobe cachedGlobe = globeCache[skillIdx];
+
+
+        int startingXp = Experience.getXpForLevel(currentLevel);
+        int goalXp = currentLevel + 1 <= Experience.MAX_VIRT_LEVEL ? Experience.getXpForLevel(currentLevel + 1) : -1;
+
+    }
+
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged event)
+    {
+        switch (event.getGameState())
+        {
+            case HOPPING:
+            case LOGGING_IN:
+                break;
+        }
+    }
+
+}
